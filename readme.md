1ed635dc5523f54290de3b7b751bb507e968ade3
denolk <den.ozgen@gmail.com>
2024-06-04 13:24:26 +0300

    improved cli output

--- readme.md
+++ readme.md
@@ -4,16 +4,18 @@ small wrapper for [pwnedpasswords](api.pwnedpasswords.com) to check if a passwor
 
 available as a cli tool or as a library on [npm/pwnchk](https://www.npmjs.com/package/pwnchk)
 
-***note:**
-this code does not expose or send the input to any server. comparison is done locally via sha1 hash. check the official [api documentation](https://haveibeenpwned.com/API/v2#PwnedPasswords) for more details.*
+_**note:**
+this code does not expose or send the input to any server. comparison is done locally via sha1 hash. check the official [api documentation](https://haveibeenpwned.com/API/v2#PwnedPasswords) for more details._
 
 ### usage
 
-- from cli
+- **standalone cli**
 
-  `npx pwnchk <password>`
+  ```bash
+  npx pwnchk <password>
+  ```
 
-- from code
+- **from code**
 
   ```bash
   npm i pwnchk
--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,33 @@ import { checkInput } from "./checker";
 const run = async () => {
   const input = process.argv[2];
   if (!input) {
-    console.error('No input provided');
+    console.log('\x1b[31m%s\x1b[0m', '>> no input provided');
     return;
   }
 
   const result = await checkInput(input);
 
   if (result.error) {
-    console.error(result.error);
+    console.log('\x1b[31m%s\x1b[0m', result.error);
   }
   else {
-    console.table(result);
+
+    if (result.match) {
+      console.log('\x1b[31m%s\x1b[0m', '>> pwned - input hash is found in the pwn list');
+    }
+    else {
+      console.log('\x1b[32m%s\x1b[0m', '>> input hash did not found in the pwn list');
+    }
+
+    console.log('');
+    console.log('hash:'.padStart(12, ' '), result.hash);
+    console.log('hashIndex:'.padStart(12, ' '), result.hashIndex);
+    console.log('hashSuffix:'.padStart(12, ' '), result.hashSuffix);
+    console.log('url:'.padStart(12, ' '), result.url);
+    console.log('views:'.padStart(12, ' '), result.views);
+    console.log('match:'.padStart(12, ' '), result.match);
+
+    // console.table(result);
   }
 };
 
